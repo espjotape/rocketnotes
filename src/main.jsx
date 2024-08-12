6645607d2a79ef4028f93c609d193234a802b06b
João Pedro <joaope.espindola2005@gmail.com>
2024-08-12 16:49:32 -0300

    Global Style

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,16 @@ import React  from 'react'
 import ReactDOM  from 'react-dom/client'
 import { Details } from './pages/Details/index.jsx'
 
+import GlobalStyles from "./styles/global.js"
+
 import { ThemeProvider } from 'styled-components' 
 import theme from './styles/theme.js'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+      <GlobalStyles />
       <Details />
     </ThemeProvider>
   </React.StrictMode>,
