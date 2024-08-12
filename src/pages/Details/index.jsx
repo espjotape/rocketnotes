d67bd4292e3ef1caff51e74b1a3bd13dfe3c4b5f
João Pedro <joaope.espindola2005@gmail.com>
2024-08-12 18:22:31 -0300

    properties

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,10 +1,14 @@
 import { Container } from "./styles"
+import { Button } from "../../components/Button"
 
 export function Details(){
   return (
     <Container>
     <h1>Hello World!</h1>
     <span>João Pedro</span>
+    <Button title="Login" loading/>
+    <Button title="Cadastrar" />
+    <Button title="Voltar" />
     </Container>
   )
 }
