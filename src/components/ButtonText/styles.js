756aa2cd6ec8485058f9f5fdbdc367e794042a76
João Pedro <joaope.espindola2005@gmail.com>
2024-08-13 13:38:48 -0300

    ButtonText component

---
+++ src/components/ButtonText/styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const Container = styled.button`
+  border: none;
+  background: none;
+
+  color: ${({ theme }) => theme.COLORS.ORANGE};
+  font-size: 16px;
+`;
--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,7 +1,8 @@
 import { Container, Links } from "./styles"
 
-import { Button } from "../../components/Button"
 import { Header } from "../../components/Header"
+import { ButtonText } from "../../components/ButtonText"
+import { Button } from "../../components/Button"
 import { Section } from "../../components/Section"
 import { Tag } from "../../components/Tag"
 
@@ -9,6 +10,9 @@ export function Details(){
   return (
     <Container>
       <Header></Header>
+
+      <ButtonText title="Excluir nota"/>
+
       <Section title="Links úteis">
         <Links>
           <li><a href="#">https://rocketseat.com.br/</a></li>
@@ -20,6 +24,7 @@ export function Details(){
        <Tag title="express"></Tag>
        <Tag title="node"></Tag>
       </Section>
+
       <Button title="Voltar" />
     </Container>
   )
