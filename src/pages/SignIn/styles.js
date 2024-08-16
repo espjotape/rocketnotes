5fd2251d602da2cdc84820e732566ca6895fe5cf
João Pedro <joaope.espindola2005@gmail.com>
2024-08-16 14:13:32 -0300

    Login form

--- src/pages/SignIn/styles.js
+++ src/pages/SignIn/styles.js
@@ -4,5 +4,40 @@ export const Container = styled.div`
   height: 100vh;
   
   display: flex;
+  align-items: stretch;
 
+ 
+`
+
+export const Form = styled.div`
+  padding: 0 136px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+
+  > h1 {
+    font-size: 48px;
+    margin-top: 18px;
+    color: ${({ theme }) => theme.COLORS.ORANGE};
+  }
+
+  > p {
+    font-size: 14px;
+    color: ${({ theme }) => theme.COLORS.GRAY_100};
+    margin-bottom: 48px;
+  }
+
+  > h2 {
+    font-size: 24px;
+    margin-bottom: 48px;
+  }
+
+  > a {
+    margin-top: 70px;
+    color: ${({ theme }) => theme.COLORS.ORANGE};
+
+  }
 `
