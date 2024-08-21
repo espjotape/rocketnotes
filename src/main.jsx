91b600e619419ef107a31699c42165c68e3ed452
João Pedro <joaope.espindola2005@gmail.com>
2024-08-21 15:23:42 -0300

    Declarative Routing for React

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,7 @@
 import React  from 'react'
 import ReactDOM  from 'react-dom/client'
 
-import { Details } from './pages/Details/index.jsx'
-import { Home } from './pages/Home/index.jsx'
-import { SignIn } from './pages/SignIn/index.jsx'
-import { SignUp } from './pages/SingUp/index.jsx'
-import { Profile } from './pages/Profile/index.jsx'
-import { New } from './pages/New/index.jsx'
+import { Routes } from './routes/index.jsx'
 
 import GlobalStyles from "./styles/global.js"
 
@@ -18,7 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <New/> 
+      <Routes/> 
     </ThemeProvider>
   </React.StrictMode>,
 )
